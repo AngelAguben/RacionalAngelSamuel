545e5ba7537dcf4e242044d7f4e7945954d253db
samuel <samubp10@gmail.com>
2020-12-10 13:10:04 +0100

    Crea el constructor parametrizado y los getters

--- src/main/java/angel/racionalangelsamuel/Racional.java
+++ src/main/java/angel/racionalangelsamuel/Racional.java
@@ -17,5 +17,21 @@ public class Racional {
         this.a = 1;
         this.b = 1;
     }
-    
+
+    public Racional(int a, int b) {
+        this.a = a;
+        this.b = b;
+        if (b == 0) {
+            this.b = 1;
+        }
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
 }
