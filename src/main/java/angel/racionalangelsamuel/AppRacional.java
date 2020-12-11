d16e8c8101233c2a6aeb26a458646556f6261432
samuel <samubp10@gmail.com>
2020-12-11 10:50:28 +0100

    Pruebas del decimalFormat

--- src/main/java/angel/racionalangelsamuel/AppRacional.java
+++ src/main/java/angel/racionalangelsamuel/AppRacional.java
@@ -5,6 +5,8 @@
  */
 package angel.racionalangelsamuel;
 
+import java.text.DecimalFormat;
+
 /**
  *
  * @author Samuel
@@ -20,7 +22,7 @@ public class AppRacional {
         Racional alea = new Racional();
         // Crearemos un objeto llamado divi para guardar la división a realizar
         Racional divi = new Racional();
-
+        DecimalFormat df = new DecimalFormat("0.00");
         // Guardaremos en alea un número Racional aleatorio
         alea = Racional.aleatorio();
         // Mostraremos por pantalla el objeto alea
@@ -36,5 +38,13 @@ public class AppRacional {
         z.sumar(k);
         // Mostraremos por pantalla el resultado de la suma
         System.out.println("Suma = " + z);
+//        df.format(z.getA());
+//        df.format(z.getB());
+//        df.format(alea.getB());
+//        df.format(alea.getA());
+        System.out.println(alea.getB());
+        System.out.printf("numero es %.2f", alea.getB());
+//        System.out.println(df.format(k));
+//        System.out.println(df.format(alea));
     }
 }
